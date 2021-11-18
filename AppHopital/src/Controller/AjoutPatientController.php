a7c19da222c6adda738932b3153eb9cc24df6427
Vonkuro <vonkuro@gmail.com>
2021-11-18 17:21:06 +0100

    80% du formulaire d'ajout de patient

--- AppHopital/src/Controller/AjoutPatientController.php
+++ AppHopital/src/Controller/AjoutPatientController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Patient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 
 class AjoutPatientController extends AbstractController
 {
     #[Route('/ajout/patient', name: 'ajout_patient')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $patient = new Patient();
+
+        $form = $this->createFormBuilder($patient)
+                    ->add('nom')
+                    ->add('prenom')
+                    ->add('dateNaissance', BirthdayType::class, [
+                        'format' => 'dd-MM-yyyy', 'attr' => [
+                            'required' => false
+                        ]
+                    ])
+                    ->add('lieuNaissance')
+                    ->add('numeroSS')
+                    ->add('probleme')
+                    ->add('description')
+                    
+                    ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+        }
+        dump($patient);
+
         return $this->render('ajout_patient/index.html.twig', [
-            'controller_name' => 'AjoutPatientController',
+            'controller_name' => 'AjoutPatientController', 'formPatient' => $form->createView()
         ]);
     }
 }
