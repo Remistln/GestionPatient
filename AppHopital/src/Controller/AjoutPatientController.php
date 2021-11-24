5b3a2cd7a854f48625a85d618f022f352ef18d61
Vonkuro <vonkuro@gmail.com>
2021-11-24 17:22:05 +0100

    Le POST de Patient 80% operationel, manque service et lit

--- AppHopital/src/Controller/AjoutPatientController.php
+++ AppHopital/src/Controller/AjoutPatientController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Patient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
@@ -11,12 +12,11 @@ use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 class AjoutPatientController extends AbstractController
 {
     #[Route('/ajout/patient', name: 'ajout_patient')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $patient = new Patient();
         
         $form = $this->createFormBuilder($patient)
-                    ->setAction("http://localhost:8080/post/patient")
                     ->add('nom')
                     ->add('prenom')
                     ->add('dateNaissance', BirthdayType::class, [
@@ -31,6 +31,29 @@ class AjoutPatientController extends AbstractController
                     
                     ->getForm();
 
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            $data = $request->request->get('form');
+            $data["dateNaissance"] = $data["dateNaissance"]["year"] . '-' . $data["dateNaissance"]["month"] . '-' . $data["dateNaissance"]["day"] ;
+            $data["numeroSS"] = intval($data["numeroSS"]); 
+            dump($data);
+            
+            $donneesPatient = json_encode($data,true);
+            dump($donneesPatient);
+            
+            $requettePatient = curl_init('http://localhost:8000/api/patients');
+
+            curl_setopt($requettePatient, CURLOPT_POSTFIELDS, $donneesPatient);
+            curl_setopt($requettePatient, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+            curl_setopt($requettePatient, CURLOPT_RETURNTRANSFER, true);
+
+            $retourApi = curl_exec($requettePatient);
+            curl_close($requettePatient);
+            
+        }
 
         return $this->render('ajout_patient/index.html.twig', [
             'controller_name' => 'AjoutPatientController',
