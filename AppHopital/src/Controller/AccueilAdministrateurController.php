4d4bc70cb62258066a990487ecf7ed4af255a87a
Vonkuro <vonkuro@gmail.com>
2021-12-01 21:52:34 +0100

    Tableaux des quatres entitiés disponible pour le twig

--- AppHopital/src/Controller/AccueilAdministrateurController.php
+++ AppHopital/src/Controller/AccueilAdministrateurController.php
@@ -5,14 +5,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\TableauPatient;
+use App\Service\TableauLit;
+use App\Service\TableauInfirmier;
+use App\Service\TableauAdmin;
+use App\Service\TableauService;
 
 class AccueilAdministrateurController extends AbstractController
 {
     #[Route('/accueil/administrateur', name: 'accueil_administrateur')]
     public function index(): Response
     {
+        $patients = new TableauPatient;
+        $tableauPatients = $patients->GetPatients();
+
+        $lits = new TableauLit;
+        $tableauLits = $lits->GetLits();
+        
+        $infirmiers = new TableauInfirmier;
+        $tableauInfirmiers = $infirmiers->GetInfirmiers();
+
+        $admins = new TableauAdmin;
+        $tableauAdmins = $admins->GetAdmins();
+        
+        $service = new TableauService;
+        $tableauServices = $service->GetServices();
+
         return $this->render('accueil_administrateur/index.html.twig', [
             'controller_name' => 'AccueilAdministrateurController',
+            'tableauPatients' => $tableauPatients,
+            'tableauLits' => $tableauLits, 'tableauServices' => $tableauServices,
+            'tableauInfirmiers' => $tableauInfirmiers, 'tableauAdmins' => $tableauAdmins
         ]);
     }
 }
