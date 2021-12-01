85f67faeeddcd27027778006adabcf6c5f9d0eae
Vonkuro <vonkuro@gmail.com>
2021-12-01 22:39:48 +0100

    le service peut être POST

--- AppHopital/src/Controller/AjoutPatientController.php
+++ AppHopital/src/Controller/AjoutPatientController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Patient;
+use App\Entity\Service;
+use App\Service\TableauService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class AjoutPatientController extends AbstractController
 {
@@ -15,10 +18,22 @@ class AjoutPatientController extends AbstractController
     public function index(Request $request): Response
     {
         $patient = new Patient();
+
+        $serviceGet = new TableauService;
+        $entiteesService = $serviceGet->GetServices();
         
+        
+
+        dump($entiteesService);
         $form = $this->createFormBuilder($patient)
                     ->add('nom')
                     ->add('prenom')
+                    ->add('service', ChoiceType::class, [
+                        'mapped' => false,
+                        'choices'  => $entiteesService,
+                        'choice_label' => 'label'
+                    ]
+                    )
                     ->add('dateNaissance', BirthdayType::class, [
                         'format' => 'yyyy-MM-dd', 'attr' => [
                             'required' => false
@@ -37,19 +52,21 @@ class AjoutPatientController extends AbstractController
         {
 
             $data = $request->request->get('form');
+            dump($data);
             $data["dateNaissance"] = $data["dateNaissance"]["year"] . '-' . $data["dateNaissance"]["month"] . '-' . $data["dateNaissance"]["day"] ;
-            $data["numeroSS"] = intval($data["numeroSS"]); 
-            
-            
-            $donneesPatient = json_encode($data,true);
-            
+            $data["numeroSS"] = intval($data["numeroSS"]);
+
+            $data["service"] = $entiteesService[$data["service"]]->getId();
+
+            $donneesPatient = json_encode($data, JSON_UNESCAPED_SLASHES, true);
+
             
             $requettePatient = curl_init('http://localhost:8000/api/patients');
 
             curl_setopt($requettePatient, CURLOPT_POSTFIELDS, $donneesPatient);
             curl_setopt($requettePatient, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
             curl_setopt($requettePatient, CURLOPT_RETURNTRANSFER, true);
-
+            dump($requettePatient);
             $retourApi = curl_exec($requettePatient);
             curl_close($requettePatient);
             
