035a18c17521ac454c15dc96ac8337b24634e84c
Rémi Staelen <remistperso@gmail.com>
2021-12-02 19:56:55 +0100

    Code Ajout admin et infirmier operationnel

--- AppHopital/src/Controller/AjoutAdminController.php
+++ AppHopital/src/Controller/AjoutAdminController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Admin;
+use App\Service\TableauAdmin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AjoutAdminController extends AbstractController
 {
     #[Route('/ajout/admin', name: 'ajout_admin')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $admin = new Admin();
+        $adminGet = new TableauAdmin;
+        $entiteesAdmin = $adminGet->GetAdmins();
+        dump($entiteesAdmin);
+
+        $form = $this->createFormBuilder($admin)
+            ->add('nom')
+            ->add('prenom')
+            ->add('identifiant')
+            ->add('mdp')
+            ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $request->request->get('form');
+            dump($data);
+            $donneesAdmin = json_encode($data, JSON_UNESCAPED_SLASHES, true);
+            $requetteAdmin = curl_init('http://localhost:8000/api/admins');
+
+            curl_setopt($requetteAdmin, CURLOPT_POSTFIELDS, $donneesAdmin);
+            curl_setopt($requetteAdmin, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+            curl_setopt($requetteAdmin, CURLOPT_RETURNTRANSFER, true);
+            dump($requetteAdmin);
+            $retourApi = curl_exec($requetteAdmin);
+            curl_close($requetteAdmin);
+        }
+
+
         return $this->render('ajout_admin/index.html.twig', [
             'controller_name' => 'AjoutAdminController',
+            'formAdmin' => $form->createView(),
         ]);
     }
 }
--- AppHopital/src/Controller/AjoutInfirmierController.php
+++ AppHopital/src/Controller/AjoutInfirmierController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Infirmier;
+use App\Service\TableauInfirmier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AjoutInfirmierController extends AbstractController
 {
     #[Route('/ajout/infirmier', name: 'ajout_infirmier')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $infirmier = new Infirmier();
+        $infirmierGet = new TableauInfirmier();
+        $entiteesInfirmier = $infirmierGet->GetInfirmiers();
+        dump($entiteesInfirmier);
+
+        $form = $this->createFormBuilder($infirmier)
+            ->add('nom')
+            ->add('prenom')
+            ->add('identifiant')
+            ->add('mdp')
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $data = $request->request->get('form');
+            dump($data);
+            $donneesInfirmier = json_encode($data, JSON_UNESCAPED_SLASHES, true);
+            $requestInfirmier = curl_init('http://localhost:8000/api/infirmiers');
+
+            curl_setopt($requestInfirmier, CURLOPT_POSTFIELDS, $donneesInfirmier);
+            curl_setopt($requestInfirmier, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+            curl_setopt($requestInfirmier, CURLOPT_RETURNTRANSFER, true);
+            dump($requestInfirmier);
+            $retourApi = curl_exec($requestInfirmier);
+            curl_close($requestInfirmier);
+        }
+
+
         return $this->render('ajout_infirmier/index.html.twig', [
             'controller_name' => 'AjoutInfirmierController',
+            'formInfirmier' => $form->createView(),
         ]);
     }
 }
