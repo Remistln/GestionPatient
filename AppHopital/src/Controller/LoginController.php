0b38176e591aef49915077716c7a05e3ee8ac313
Rémi Staelen <remistperso@gmail.com>
2021-12-04 17:53:23 +0100

    recup des admins quand on submit le login

--- AppHopital/src/Controller/LoginController.php
+++ AppHopital/src/Controller/LoginController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Admin;
+use App\Service\TableauAdmin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LoginController extends AbstractController
 {
     #[Route('/', name: 'login')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $form = $this->createFormBuilder()
+            ->add('identifiant')
+            ->add('mdp')
+            ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $request->request->get('form');
+
+            $admins = new TableauAdmin;
+            $tableauAdmins = $admins->GetAdmins();
+            dump($tableauAdmins);
+        }
+
+
+
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
+            'formLogin' => $form->createView(),
         ]);
     }
 }
