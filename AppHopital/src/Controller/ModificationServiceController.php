449d2cd60dc5e727370c1ac87c3b97d5b4d3a13a
Vonkuro <vonkuro@gmail.com>
2021-12-04 21:45:08 +0100

    Modification d'un Service Fait

--- AppHopital/src/Controller/ModificationServiceController.php
+++ AppHopital/src/Controller/ModificationServiceController.php
@@ -4,15 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\TableauService;
+use App\Entity\Service;
 
 class ModificationServiceController extends AbstractController
 {
-    #[Route('/modification/service', name: 'modification_service')]
-    public function index(): Response
+    #[Route('/modification/service/{id}', name: 'modification_service')]
+    public function index($id,Request $request): Response
     {
+        $manager = new TableauService;
+        $service = $manager->GetService($id);
+
+        $form= $this->createFormBuilder($service)
+                    ->add('label')
+
+                    ->getForm();
+
+                    
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $data = $request->request->get('form');
+            
+            $retourAPI = $manager->PutService($id,$data);
+        }
+
         return $this->render('modification_service/index.html.twig', [
             'controller_name' => 'ModificationServiceController',
+            'formService' => $form->createView(),
         ]);
     }
 }
