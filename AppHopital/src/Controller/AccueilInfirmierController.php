0593e9e48316c2e9356f526f3fe5f968369229f7
Rémi Staelen <remistperso@gmail.com>
2021-12-05 15:41:54 +0100

    Acces a acceuil infirmier impossible si on est pas connecté

--- AppHopital/src/Controller/AccueilInfirmierController.php
+++ AppHopital/src/Controller/AccueilInfirmierController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\TableauPatient;
@@ -15,31 +16,34 @@ use App\Service\TableauService;
 class AccueilInfirmierController extends AbstractController
 {
     #[Route('/accueil/infirmier', name: 'accueil_infirmier')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $patients = new TableauPatient;
-        $tableauPatients = $patients->GetPatients();
+        $session = $request->getSession();
+        $role = $session->get('role');
+        if ($role == 'ROLE_ADMIN' || $role == 'ROLE_USER'){
+            $patients = new TableauPatient;
+            $tableauPatients = $patients->GetPatients();
 
-        $lits = new TableauLit;
-        $tableauLits = $lits->GetLits();
+            $lits = new TableauLit;
+            $tableauLits = $lits->GetLits();
 
-/*
-        $infirmiers = new TableauInfirmier;
-        $tableauInfirmiers = $infirmiers->GetInfirmiers();
-
-        $admins = new TableauAdmin;
-        $tableauAdmins = $admins->GetAdmins();
-*/
-        $service = new TableauService;
-        $tableauServices = $service->GetServices();
+            $service = new TableauService;
+            $tableauServices = $service->GetServices();
 
 
-        return $this->render('accueil_infirmier/index.html.twig', [
-            'controller_name' => 'AccueilInfirmierController',
-            'title'=> 'Accueil', 'tableauPatients' => $tableauPatients,
-            'tableauLits' => $tableauLits, 'tableauServices' => $tableauServices,
-        ]);
+            return $this->render('accueil_infirmier/index.html.twig', [
+                'controller_name' => 'AccueilInfirmierController',
+                'title'=> 'Accueil',
+                'tableauPatients' => $tableauPatients,
+                'tableauLits' => $tableauLits,
+                'tableauServices' => $tableauServices,
+            ]);
+        }else{
+            return $this->render('access_denied/index.html.twig', [
+                'controller_name' => 'AccueilInfirmierController',
+                'error' => "Vous n'êtes pas autorisé à aller sur cette page"
+            ]);
+        }
     }
-
 
 }
