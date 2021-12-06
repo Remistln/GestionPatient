c073a3a61232e705ffde550c65dea4bde992b776
Rémi Staelen <remistperso@gmail.com>
2021-12-06 22:28:49 +0100

    redirection apres modification

--- AppHopital/src/Controller/ModificationLitController.php
+++ AppHopital/src/Controller/ModificationLitController.php
@@ -14,38 +14,52 @@ class ModificationLitController extends AbstractController
     #[Route('/modification/lit/{id}', name: 'modification_lit')]
     public function index($id, Request $request): Response
     {
-        $managerLit = new TableauLit;
-        $lit = $managerLit->GetLit($id);
+        $session = $request->getSession();
+        $role = $session->get('role');
+        if ($role == 'ROLE_ADMIN' || $role == 'ROLE_USER'){
+            $managerLit = new TableauLit;
+            $lit = $managerLit->GetLit($id);
 
-        $form = $this ->createFormBuilder($lit)
-                      ->add('numero')
-                      ->add('chambre')
-                      ->add('longueur')
-                      ->add('largeur')
-                      ->add('etat')
+            $form = $this ->createFormBuilder($lit)
+                ->add('numero')
+                ->add('chambre')
+                ->add('longueur')
+                ->add('largeur')
+                ->add('etat')
 
-                      ->getForm();
+                ->getForm();
 
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $data = $request->request->get('form');
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid())
+            {
+                $data = $request->request->get('form');
 
-            $data["numero"] = intval($data["numero"]);
-            $data["chambre"] = intval($data["chambre"]); 
-            $data["longueur"] = floatval($data["longueur"]); 
-            $data["largeur"] = floatval($data["largeur"]); 
-            $data["etat"] = boolval($data["etat"]); 
+                $data["numero"] = intval($data["numero"]);
+                $data["chambre"] = intval($data["chambre"]);
+                $data["longueur"] = floatval($data["longueur"]);
+                $data["largeur"] = floatval($data["largeur"]);
+                $data["etat"] = boolval($data["etat"]);
 
-           
-            $retourApi = $managerLit->PutLit($id, $data);
-            return $this->redirectToRoute('login');
-            
+
+                $retourApi = $managerLit->PutLit($id, $data);
+                if ($role == 'ROLE_ADMIN'){
+                    return $this->redirectToRoute('accueil_administrateur');
+                }else{
+                    return $this->redirectToRoute('accueil_infirmier');
+                }
+
+            }
+            return $this->render('modification_lit/index.html.twig', [
+                'controller_name' => 'ModificationLitController',
+                'formLit'=> $form->createView(),
+                'role' => $role
+            ]);
+        }else{
+            return $this->render('access_denied/index.html.twig', [
+                'controller_name' => 'ModificationLitController',
+                'error' => "Vous n'êtes pas autorisé à aller sur cette page"
+            ]);
         }
-        
-        return $this->render('modification_lit/index.html.twig', [
-            'controller_name' => 'ModificationLitController',
-            'formLit'=> $form->createView(),
-        ]);
+
     }
 }
--- AppHopital/src/Controller/ModificationPatientController.php
+++ AppHopital/src/Controller/ModificationPatientController.php
@@ -18,42 +18,57 @@ class ModificationPatientController extends AbstractController
     #[Route('/modification/patient/{id}', name: 'modification_patient')]
     public function index(int $id,Request $request): Response
     {
-        $managerPatient = new TableauPatient();
-        $patient = $managerPatient->GetPatient($id);
+        $session = $request->getSession();
+        $role = $session->get('role');
+        if ($role == 'ROLE_ADMIN' || $role == 'ROLE_USER'){
+            $managerPatient = new TableauPatient();
+            $patient = $managerPatient->GetPatient($id);
 
-        $form = $this->createFormBuilder($patient)
-                    ->add('nom')
-                    ->add('prenom')
-                    ->add('dateNaissance', BirthdayType::class, [
-                        'format' => 'yyyy-MM-dd', 'attr' => [
-                            'required' => false
-                        ]
-                    ])
-                    ->add('lieuNaissance')
-                    ->add('numeroSS')
-                    ->add('probleme')
-                    ->add('description')
+            $form = $this->createFormBuilder($patient)
+                ->add('nom')
+                ->add('prenom')
+                ->add('dateNaissance', BirthdayType::class, [
+                    'format' => 'yyyy-MM-dd', 'attr' => [
+                        'required' => false
+                    ]
+                ])
+                ->add('lieuNaissance')
+                ->add('numeroSS')
+                ->add('probleme')
+                ->add('description')
 
-                    ->getForm();
+                ->getForm();
 
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid())
-        {
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid())
+            {
 
-            $data = $request->request->get('form');
-            
-            $data["dateNaissance"] = $data["dateNaissance"]["year"] . '-' . $data["dateNaissance"]["month"] . '-' . $data["dateNaissance"]["day"] ;
-            $data["numeroSS"] = intval($data["numeroSS"]);
-            
-            $tableauPatient = new TableauPatient;
-            $retourApi = $tableauPatient->PutPatient($id,$data);
-            return $this->redirectToRoute('login');
+                $data = $request->request->get('form');
+
+                $data["dateNaissance"] = $data["dateNaissance"]["year"] . '-' . $data["dateNaissance"]["month"] . '-' . $data["dateNaissance"]["day"] ;
+                $data["numeroSS"] = intval($data["numeroSS"]);
+
+                $tableauPatient = new TableauPatient;
+                $retourApi = $tableauPatient->PutPatient($id,$data);
+                if ($role == 'ROLE_ADMIN'){
+                    return $this->redirectToRoute('accueil_administrateur');
+                }else{
+                    return $this->redirectToRoute('accueil_infirmier');
+                }
+            }
+
+            return $this->render('modification_patient/index.html.twig', [
+                'controller_name' => 'ModificationPatientController',
+                'formPatient' => $form->createView(),
+                'role' => $role
+            ]);
+        }else{
+            return $this->render('access_denied/index.html.twig', [
+                'controller_name' => 'ModificationLitController',
+                'error' => "Vous n'êtes pas autorisé à aller sur cette page"
+            ]);
         }
 
-        return $this->render('modification_patient/index.html.twig', [
-            'controller_name' => 'ModificationPatientController',
-            'formPatient' => $form->createView(),
-        ]);
     }
 
     
