ea4ac7ca77cc8619205e34ef9ee1f734afde7a4e
ayanajare <aya.najare@epsi.fr>
2022-03-21 18:24:28 +0100

    Les dépendances

--- ApiHopital/config/bundles.php
+++ ApiHopital/config/bundles.php
@@ -11,4 +11,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
