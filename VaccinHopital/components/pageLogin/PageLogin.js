96b152030802ffd94416567a19cd17fe58a9e26b
Vonkuro <vonkuro@gmail.com>
2022-03-23 22:07:32 +0100

    Page Login : lien entre bouton et input & plan pour verifier le mdp

--- VaccinHopital/components/pageLogin/PageLogin.js
+++ VaccinHopital/components/pageLogin/PageLogin.js
@@ -1,27 +1,62 @@
 import { Text, Block, Button, Input } from 'galio-framework';
 import { StyleSheet } from 'react-native';
 
-export default function PageLogin() {
-  return (
-    <Block flex row>
-        <Block flex row = {false}>
-            <Block flex ></Block>
-            <Block center flex = {2} >
-                <Text h2>Login</Text>
+export default class PageLogin extends Component {
+
+    constructor()
+    {
+        super();
+        this.state = {
+            identifiant: '',
+            mdp: ''
+        }
+    }
+
+    connect()
+    {
+        /*
+            Plan:
+            mettre a jour ma bdd
+            coder un appel API pour obtenir le secretaire
+                appel sync
+                si possible en fonction de l'identifiant
+            verifier le mdp
+            passer à la page Acceuil si mdp correct
+                attention debut d'architecture d'app nécessaire
+        */
+        compare(string, hash, function(err, res)
+        {
+            if (res)
+            {
+
+            };
+        }
+        );
+    };
+
+    render(){
+        return (
+            <Block flex row>
+                <Block flex row = {false}>
+                    <Block flex ></Block>
+                    <Block center flex = {2} >
+                        <Text h2>Login</Text>
+                    </Block>
+                    <Block left middle flex = {2} width="75%" style ={styles.millieu}>
+                        <Text h3 >Nom</Text>
+                        <Input onChangeText={(text) => this.setState({identifiant: text})}></Input>
+                        <Text h3>Mot de Passe</Text>
+                        <Input secureTextEntry={true} onChangeText={(text) => this.setState({mdp: text})}></Input>
+                    </Block>
+                    <Block flex></Block>
+                    <Block bottom flex  left >
+                        <Button bottom onPress={() => connect }>Connexion</Button>
+                    </Block>
+                </Block>
             </Block>
-            <Block left middle flex = {2} width="75%" style ={styles.millieu}>
-                <Text h3 >Nom</Text>
-                <Input></Input>
-                <Text h3>Mot de Passe</Text>
-                <Input secureTextEntry={true}></Input>
-            </Block>
-            <Block flex></Block>
-            <Block bottom flex  left >
-                <Button bottom>Connexion</Button>
-            </Block>
-        </Block>
-    </Block>
-  );
+        );
+    }
+  
 }
 
 const styles = StyleSheet.create({
@@ -34,6 +69,3 @@ const styles = StyleSheet.create({
     },
   });
   
-
-var sel = "protectMè4Mù"
-
