6f2f4a997a022f992c6b54cb04bf0156e0c046d4
Rémi Staelen <remistperso@gmail.com>
2022-03-23 19:31:27 +0100

    modif api

---
+++ VaccinHopital/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3ce1d667c3e6d157e255578e833d3f59ca99d850',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3ce1d667c3e6d157e255578e833d3f59ca99d850',
+            'dev_requirement' => false,
+        ),
+    ),
+);
