28d14afd1fd3ee953737ee04cc77e79b0bb4a59b
Vonkuro <vonkuro@gmail.com>
2022-04-14 17:58:07 +0200

    fonctions d'affichage de boutons jours du mois : une ligne de jours depuis une date pour un nombre de jour donné, la ligne de la première semaine d'un mois, la ligne pour la dernière semaine d'un mois.

--- VaccinHopital/components/pageAgenda/agendaJours/AgendaJours.js
+++ VaccinHopital/components/pageAgenda/agendaJours/AgendaJours.js
@@ -9,22 +9,18 @@ export default function AgendaJours() {
                 <Text>[mois] [annee]</Text>
                 <Button onlyIcon icon="right" iconFamily="antdesign" iconSize={30} color="black" iconColor="#fff" style={{ width: 40, height: 40 }}></Button>
             </Block>
-            <Block style = {styles.jours} >
-                {buttonsSemaine(3)}
-            </Block>
-            <Block style = {styles.jours} >
-                {buttonsSemaine(3)}
-            </Block>
-            <Block style = {styles.jours} >
-                {buttonsSemaine(3)}
-            </Block>
+
+            {buttonsPremiereSemaine(3,2022)}
+
+            {buttonsDernièreSemaine(3,2022)}
+
         </Block>
     );// pour le calendrier : faire une boucle de bouton, les ranger dans une liste, leur attribuer un numéro en fonction du mois
 }
 
-function buttonsSemaine(nombreLundi){
+function buttonsSemaine(nombreLundi, nombreJours){
     let nombresSemaine = [];
-    for (let index = 0; index < 7; index++) {
+    for (let index = 0; index < nombreJours; index++) {
         nombresSemaine.push(nombreLundi + index);
     };
     let buttonsListe=nombresSemaine.map((nombre)=>{
@@ -36,9 +32,65 @@ function buttonsSemaine(nombreLundi){
     return buttonsListe;
 }
 
-/* créé une fonction pour une ligne de 7 boutons avec nombre les premiers et correctes chiffre du mois 
+function buttonsPremiereSemaine(moisNombre, anneeNombre){
+    let moisDate = new Date();
+
+    moisDate.setFullYear(anneeNombre, moisNombre, 1);
+    let premierJourMois = moisDate.getDay();
+
+    if (premierJourMois == 0)
+    {
+        premierJourMois = 7;
+    }
+    
+    moisDate.setFullYear(anneeNombre, moisNombre, 0);
+    const dernierJourMois = moisDate.getDate() - premierJourMois +2;
+
+    return (
+        <Block style = {styles.jours}>
+            {buttonsSemaine(dernierJourMois,premierJourMois-1)}
+            {buttonsSemaine(1,8-premierJourMois)}
+        </Block>
+    )
+}
+
+function buttonsDernièreSemaine(moisNombre, anneeNombre){
+    let moisDate = new Date();
+    moisDate.setFullYear(anneeNombre, moisNombre+1, 0);
+    const dernierJourMoisDate = moisDate.getDate();
+    let dernierJourMoisSemaine = moisDate.getDay();
+
+    if (dernierJourMoisSemaine == 0)
+    {
+        dernierJourMoisSemaine = 7;
+    }
+
+    //gestion du cas d'un février contenue dans 4 semaines au lieu de 5 semaines
+    if (moisNombre == 1 && dernierJourMoisSemaine == 7)
+    {
+        return (
+            <Block style = {styles.jours}>
+            {buttonsSemaine(1,7)}
+            </Block>
+        )
+    };
+
+    return (
+        <Block style = {styles.jours}>
+            {buttonsSemaine(dernierJourMoisDate - dernierJourMoisSemaine + 1,dernierJourMoisSemaine)}
+            {buttonsSemaine(1,7-dernierJourMoisSemaine)}
+        </Block>
+    )
+
+}
+
+function premierLundiMois(moisNombre, anneeNombre)
+{
+    
+}
+/* créé une fonction pour une ligne de 7 boutons avec nombre les premiers et correctes chiffre du mois  Fait
 créé une fonction pour une ligne de 7 boutons avec nombre croissant depuis un index donnéé Fait
-créé une fonction pour une ligne de 7 boutons avec nombre les derniers et correctes chiffre du mois 
+créé une fonction pour une ligne de 7 boutons avec nombre les derniers et correctes chiffre du mois Fait
 créé une fonction qui affiche les 5 lignes */
 
 
