d44429bff70551cf7a7d708b4b51498f413a85c6
Vonkuro <vonkuro@gmail.com>
2022-04-18 10:29:32 +0200

    [WIP] recherche d'une bonne méthode pour afficher les boutons mois

--- VaccinHopital/components/pageAgenda/agendaMois/AgendaMois.js
+++ VaccinHopital/components/pageAgenda/agendaMois/AgendaMois.js
@@ -1,20 +1,51 @@
-import {Block, Button, Icon } from 'galio-framework';
+import {Text, Block, Button, Icon } from 'galio-framework';
 import { StyleSheet } from "react-native";
 import React, { useState } from 'react';
 
 export default function AgendaMois() {
+    let anneeInitiale = 2022;
+    
+    const [annee, setAnnee] = useState(anneeInitiale);
 
     return(
         <Block style = {styles.block}>
             <Block style = {styles.entete}>
-
+                <Button 
+                        onlyIcon icon="left" iconFamily="antdesign" iconSize={30} color="black" iconColor="#fff"  style={{ width: 40, height: 40 }}
+                        onPress= {() => {
+                            setAnnee(annee - 1);
+                        }}
+                        ></Button>
+                    <Text h4>{annee}</Text>
+                    <Button 
+                        onlyIcon icon="right" iconFamily="antdesign" iconSize={30} color="black" iconColor="#fff" style={{ width: 40, height: 40 }}
+                        onPress= {() => {
+                            setAnnee(annee + 1);
+                        }}
+                    ></Button>
             </Block>
             <Block style = {styles.agenda}>
-
+                {calendrier(annee)}
             </Block>
         </Block>
     );
 };
+
+
+function calendrier(annee)
+{
+    const moisListe = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    let nombre = 0;
+    let buttonsListe=moisListe.map((mois)=>{
+        nombre += 1;
+        return (
+        <Button style = {styles.jour} color="info" key={nombre}>
+            <Text color="white">{mois}</Text>
+        </Button>)
+      });
+    return buttonsListe;
+};
+
 
 
 const styles = StyleSheet.create({
