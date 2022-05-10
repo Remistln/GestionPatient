2aa33dac0b0cc16b74d273a2041a6e9e1b5a017a
Vonkuro <vonkuro@gmail.com>
2022-05-10 14:21:06 +0200

    Rendez-vous API améliorée

--- ApiHopital/src/Entity/RendezVous.php
+++ ApiHopital/src/Entity/RendezVous.php
@@ -27,38 +27,38 @@ class RendezVous
     private $id;
 
     /**
-     * @ORM\ManyToOne(targetEntity=Infirmier::class, inversedBy="rendezVous")
-     * @Groups({"rdv_read"})
-     */
-    private $infirmier;
-
-    /**
      * @ORM\OneToOne(targetEntity=Vaccin::class, cascade={"persist", "remove"})
      * @Groups({"rdv_read"})
      */
     private $vaccin;
 
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="date")
      * @Groups({"rdv_read"})
      */
     private $Date;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"rdv_read"})
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"rdv_read"})
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="time")
+     * @Groups({"rdv_read"})
+     */
+    private $heure;
+
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getInfirmier(): ?Infirmier
-    {
-        return $this->infirmier;
-    }
-
-    public function setInfirmier(?Infirmier $infirmier): self
-    {
-        $this->infirmier = $infirmier;
-
-        return $this;
     }
 
     public function getVaccin(): ?Vaccin
@@ -81,6 +81,42 @@ class RendezVous
     public function setDate(\DateTimeInterface $Date): self
     {
         $this->Date = $Date;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getHeure(): ?\DateTimeInterface
+    {
+        return $this->heure;
+    }
+
+    public function setHeure(\DateTimeInterface $heure): self
+    {
+        $this->heure = $heure;
 
         return $this;
     }
