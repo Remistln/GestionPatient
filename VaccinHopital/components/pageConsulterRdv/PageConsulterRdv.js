5d72904aff7e58990dff5d088a553676179a85ea
ayanajare <aya.najare@epsi.fr>
2022-05-25 13:42:56 +0200

    navigation de consulter à prise rdv en cours (il manque les données dynamiques)

--- VaccinHopital/components/pageConsulterRdv/PageConsulterRdv.js
+++ VaccinHopital/components/pageConsulterRdv/PageConsulterRdv.js
@@ -1,12 +1,11 @@
 import {Card, Text, Block, Button} from 'galio-framework';
 import {StyleSheet, ScrollView, SafeAreaView} from 'react-native';
-import {useNavigation, NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {useEffect, useState} from "react";
 
-export default function PageConsulterRdv({
-	                                         choosenDate = "2001-12-25"
-                                         }) {
+
+export default function PageConsulterRdv({choosenDate = "2022-06-10", navigation})
+{
 
 	const [rdvList, letRdvList] = useState([]);
 
@@ -15,8 +14,8 @@ export default function PageConsulterRdv({
 
 		//Ip de l'ordi
 		//remi : 192.168.1.14:8000
-
-		let requete = "http://192.168.1.14:8000/api/rendez_vouses?Date=" + choosenDate
+		const ip ="172.20.10.4:8000"; //ip aya
+		let requete = "http://"+ip+"/api/rendez_vouses?Date=" + choosenDate
 
 		fetch(requete, {
 			headers: {
@@ -37,50 +36,53 @@ export default function PageConsulterRdv({
 
 	return (
 		<SafeAreaView>
+			<Block style={styles.titre}>
+				<Text style={styles.TextTitre} h5>Rendez-vous le {choosenDate}</Text>
+			</Block>
+			<Block style={styles.block}>
 			<ScrollView>
-				<Block style={styles.block}>
-					<Block style={styles.titre}>
-						<Text style={styles.TextTitre} h5>Rendez-vous le {choosenDate}</Text>
-					</Block>
-					<Block style={styles.btnBlock}>
-						<Button round style={styles.button} color="primary">Ajouter un RDV</Button>
-					</Block>
-
 					{rdvList.map((rdv) => {
 						let cardTitle = rdv.nom + " " + rdv.prenom
 
 						return (
-							<Block style={styles.cardBlock}>
-								<Card
+							<Block  style={styles.cardBlock}>
+								<Card 
+
 									flex
 									borderless={false}
 									style={styles.card}
 									title={cardTitle}
 									caption={rdv.vaccin.type.label}
-								/>
+										>
+									<Button  color="warning" style={styles.btnCard}  >Annuler le RDV</Button>
+
+								</Card>
+
 							</Block>
 						)
 
 					})}
-				</Block>
+
 			</ScrollView>
+			</Block>
+			<Block style={styles.btnBlock}>
+				<Button round style={styles.button} color="primary"
+						onPress={() => navigation.navigate('PageChargementRdv')}>Ajouter un RDV</Button>
+			</Block>
 		</SafeAreaView>
 
 
 	);
 }
-/*
 
-//onPress={() => navigation.navigate('PriseDeRDV')}
-*/
 const styles = StyleSheet.create({
 	block:
 		{
 			flexDirection: "column",
-			marginTop: 50,
+			marginTop: 70,
 			width: 415,
-			height: 800,
-			// backgroundColor: "red"
+			height: 700,
+
 
 		},
 	titre:
@@ -93,26 +95,40 @@ const styles = StyleSheet.create({
 	TextTitre:
 		{
 			fontWeight: "bold",
-			marginTop: 60,
+			marginTop: 10 ,
+			height: 60,
 			textAlign: "center",
-			//  backgroundColor: "green" ,
+			zIndex: 0,
+			//backgroundColor: "green" ,
 		},
 	cardBlock:
 		{
-			//  backgroundColor : "blue",
+			//backgroundColor : "blue",
 			flex: 2,
-			height: 2,
+			height: 150,
+			zIndex: 0,
+			marginTop: 10,
+			fontSize: 20
+
+
 
 		},
 	card:
 		{
-			// backgroundColor : "green",
+			//backgroundColor : "yellow",
 			alignItems: "center",
 			height: 100,
 			marginLeft: 70,
 			marginRight: 70,
 			flex: 1,
-			marginTop: 7
+			marginTop: 7,
+			fontSize: 20
+		},
+	btnCard :
+		{
+
+			width: 100,
+			height: 40
 		},
 	btnBlock:
 		{
@@ -121,10 +137,12 @@ const styles = StyleSheet.create({
 			alignItems: "center",
 			marginTop: 3,
 
+
 		},
 	button:
 		{
 			justifyContent: 'space-evenly',
 			alignItems: "center",
+
 		}
 });
